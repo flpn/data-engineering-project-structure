85f034df7561b30a3c92bd3c95a44e2265dce122
Felipe Netto <flpncc@gmail.com>
2021-12-07 22:05:45 -0300

    Create unit tests

---
+++ tests/test_clean_columns.py
@@ -0,0 +1,19 @@
+from transformations import clean_data
+
+
+def test_should_upper_case_names():
+    columns = ['age', 'sex']
+    
+    actual = clean_data.upper_case_names(columns)
+    expected = ['AGE', 'SEX']
+
+    assert actual == expected
+
+
+def test_should_remove_whitespaces_from_names():
+    columns = ['FIRST NAME']
+
+    actual = clean_data.remove_whitespaces_from_names(columns)
+    expected = ['FIRST_NAME']
+
+    assert actual == expected
