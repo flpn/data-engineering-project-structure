b4b455d5fadfecb077838911ab7b3189ba020320
Felipe Netto <flpncc@gmail.com>
2021-12-07 23:30:08 -0300

    Glue every jobs method together

--- transformations/clean_data.py
+++ transformations/clean_data.py
@@ -7,3 +7,18 @@ def upper_case_names(names: List) -> List:
 
 def remove_whitespaces_from_names(names: List) -> List:
     return [name.strip().replace(' ', '_') for name in names]
+
+
+def clean_names(names):
+    return remove_whitespaces_from_names(upper_case_names(names))
+
+
+def run(spark, dataset_input_path, dataset_output_path):
+    input_dataframe = spark.read.csv(dataset_input_path, header=True)
+    
+    cleaned_columns = clean_names(input_dataframe.columns)
+    output_dataframe = input_dataframe.toDF(*cleaned_columns)
+
+    output_dataframe.show()
+
+    output_dataframe.write.parquet(dataset_output_path)
